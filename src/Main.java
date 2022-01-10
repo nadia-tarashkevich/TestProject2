9bdf47f845b1785cbf47487e57788e09efeb7138
Nadezhda Solovyeva <nadia.solovyeva@jetbrains.com>
2022-01-10 18:45:24 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i ++ ) {
+            if (i > 20) {
+                System.out.print("i > 20!");
+            }
+        }
+
+        for (int i = 0; i < 20; i ++ ) {
+            if (i > 20) {
+                System.out.print("i > 20!");
+            }
+        }
+
+        for (int i = 0; i < 20; i ++ ) {
+            if (i > 21) {
+                System.out.print("i > 20!");
+            }
+        }
+    }
+}
